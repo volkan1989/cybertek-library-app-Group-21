10aed1313877925c4d7015274c3320c70e133e12
Saltanat2020 <saabdubek@gmail.com>
2021-02-25 11:50:22 -0500

    Page

--- src/test/java/com/cybertek/library/pages/US6_TablesWithColumns.java
+++ src/test/java/com/cybertek/library/pages/US6_TablesWithColumns.java
@@ -1,9 +1,18 @@
 package com.cybertek.library.pages;
 
+import com.cybertek.library.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class US6_TablesWithColumns {
 
     public US6_TablesWithColumns(){
+        PageFactory.initElements(Driver.getDriver(),this);
+
+
 
     }
-
+@FindBy(xpath = "//th[@data-name]")
+    public WebElement ColumnsList;
 }
--- src/test/java/com/cybertek/library/step_definitions/US6_TablesWithColumns_StepDefinitions.java
+++ src/test/java/com/cybertek/library/step_definitions/US6_TablesWithColumns_StepDefinitions.java
@@ -1,14 +1,19 @@
 package com.cybertek.library.step_definitions;
 
+import com.cybertek.library.pages.LoginPage;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 import java.util.List;
 
 public class US6_TablesWithColumns_StepDefinitions {
+
+   LoginPage loginPage = new LoginPage();
+
     @When("The user logs in")
     public void the_user_logs_in() {
 
+        loginPage.loginToLibrary("student55");
 
     }
 
